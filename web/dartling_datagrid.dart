227c3bec2fdd0220f091d12691f6f40bf6590379
Curtis <curtis@ubuntu.(none)>
2013-04-07 16:57:19 -0400

    Address added

--- web/dartling_datagrid.dart
+++ web/dartling_datagrid.dart
@@ -8,35 +8,41 @@ void main() {
 
   var davidCurtis = new Member(memberConcept);
   davidCurtis.code = 'davidCurtis';
-  davidCurtis.firstname = 'david';
+  davidCurtis.firstname = 'Patrick';
   davidCurtis.address ='1308 Noirefontaine';
-  davidCurtis.lastname = 'Curtis';
+  davidCurtis.lastname = 'Lemieux';
   
   members.add(davidCurtis);
   
   var jeanTigana = new Member(memberConcept);
   jeanTigana.code = 'jeanTigana';
   jeanTigana.firstname = 'Jean';
+  jeanTigana.address = '145 Rue Dorval';
+  jeanTigana.lastname ='Tiguana';
   members.add(jeanTigana);
   
   var rafikBenmoussa = new Member(memberConcept);
   rafikBenmoussa.firstname = 'Rafik';
   rafikBenmoussa.lastname = 'Benmoussa';
+  rafikBenmoussa.address ='785 Av. Monparnasse';
   members.add(rafikBenmoussa);
   
   var timurRidjanovic = new Member(memberConcept);
   timurRidjanovic.firstname = 'Timur';
   timurRidjanovic.lastname = 'Ridjanovic';
+  timurRidjanovic.address ='74 Rue Royale';
   members.add(timurRidjanovic);
   
   var dzenanRidjanovic = new Member(memberConcept);
   dzenanRidjanovic.firstname = 'Dzenan';
   dzenanRidjanovic.lastname = 'Ridjanovic';
+  dzenanRidjanovic.address ='78 Av. Sainte Foy';
   members.add(dzenanRidjanovic);
   
   var lamineBazzi = new Member(memberConcept);
   lamineBazzi.firstname = 'Lamine';
   lamineBazzi.lastname = 'Baazi';
+  lamineBazzi.address = '78 Lanaudière';
   members.add(lamineBazzi);
 
 
